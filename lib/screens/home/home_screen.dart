40e5d4bea63bf76a40008cd9f4e44bca7a6ca9e6
Mitchell Chiadika <udoagwac@gmail.com>
2022-03-21 10:01:35 +0100

    full app:

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,10 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:ffi';
 
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:flutter/material.dart';
+import 'package:highlight_text/highlight_text.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
 
 class HomeScreen extends StatefulWidget {
@@ -11,10 +15,34 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final Map<String, HighlightedWord> _highlights = {
+    'Hello': HighlightedWord(
+      onTap: () => print('hello tapped'),
+      textStyle: TextStyle(
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'welcome': HighlightedWord(
+      onTap: () => print('welcome tapped'),
+      textStyle: TextStyle(
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    'test': HighlightedWord(
+      onTap: () => print('test tapped'),
+      textStyle: TextStyle(
+        color: Colors.red,
+        fontWeight: FontWeight.bold,
+      ),
+    )
+  };
+
   late stt.SpeechToText _speech;
-  bool isListening = false;
-  String text = 'Press the button to start speaking';
-  double confidence = 1.0;
+  bool _isListening = false;
+  String _text = 'Press the button to start speaking';
+  double _confidence = 1.0;
 
   @override
   void initState() {
@@ -26,8 +54,65 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('${(confidence * 100.0).toStringAsFixed(1)}%'),
+        title: Text('Accuracy: ${(_confidence * 100.0).toStringAsFixed(1)}%'),
+      ),
+      body: SingleChildScrollView(
+        reverse: true,
+        child: Container(
+          padding: EdgeInsets.fromLTRB(30, 30, 30, 150),
+          child: TextHighlight(
+            text: _text,
+            words: _highlights,
+            textStyle: TextStyle(
+              fontSize: 32,
+              color: Colors.black,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: AvatarGlow(
+        animate: _isListening,
+        glowColor: Theme.of(context).primaryColor,
+        duration: const Duration(milliseconds: 2000),
+        repeatPauseDuration: const Duration(milliseconds: 100),
+        repeat: false,
+        endRadius: 75.0,
+        child: FloatingActionButton(
+          onPressed: _listen,
+          child: Icon(_isListening ? Icons.mic : Icons.mic_none),
+        ),
       ),
     );
+  }
+
+  void _listen() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (val) => print('onStatus; $val'),
+        onError: (val) => print('onError; $val'),
+        debugLogging: true,
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: (val) => setState(
+            () {
+              _text = val.recognizedWords;
+
+              if (val.hasConfidenceRating && val.confidence > 0) {
+                _confidence = val.confidence;
+
+                print(val.recognizedWords);
+              }
+            },
+          ),
+        );
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
   }
 }
